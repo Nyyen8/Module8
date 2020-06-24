55657dbcc4f0211a356b4df05bc1ad73b572e42e
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-24 16:24:10 -0500

    test and assign_average.py All comments added

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -3,16 +3,14 @@ Program: assign_average.py
 Author: Paul Elsea
 Last Modified: 06/24/2020
 
-Program to average scores from a dictionary populated by user input.
+Program to average scores for a dictionary key chosen by the user.
 """
 
-'''This declares a dictionary, then prompts user for a group of scores to average. Results of averaged group
-of scores is printed, or error message is displayed and user is prompted for new input.
-:scores_dict: Dictionary of scores to be averaged
-:input_scores: Number of scores prior to being validated as integer.
-:num_scores: Input number of total scores to be asked for after validation.
-:input_score: Each individual score to be validated and inserted into the dictionary.
-:returns: Populated score dictionary'''
+'''This takes in a dictionary, prompts user for a group of scores to average. Results of averaged group
+of scores is returned, or an error message if user inputs invalid data.
+:input_dict: The dictionary that the user is choosing which key to work with
+:choice: The key the user will select to work with
+:returns: Averaged scores of chosen dictionary key or an error message'''
 
 def switch_average(input_dict):
     while True:
@@ -29,48 +27,19 @@ def switch_average(input_dict):
         else:
             return "I'm sorry, I didn't recognize that. Please choose either A, B, C, or D.\n"
 
+'''This prompts user for an letter choice which is returned.
+:user_choice: The key chosen by the user.
+:returns: Char or string entered by the user'''
+
 def get_input():
     user_choice = input('Please choose score set A, B, C, or D.\n')
     return user_choice
 
-def get_test_scores():
-    scores_dict = {}
-
-    input_scores = input('Please enter number of scores:\n')
-    while True:
-        try:
-            num_scores = int(input_scores)
-            break
-        except ValueError:
-            input_scores = input('Please try again and use only numerals for number of scores:\n')
-            continue
-
-
-    for score in range(num_scores):
-        while True:
-            try:
-                input_score = float(input('Please enter score #' + str(score+1) + ':\n'))
-                if 0.00 > input_score or input_score > 100.00:
-                    if input_score > 100.00:
-                        print('Score is above maximum. Please enter score between 0 and 100')
-                        continue
-                    else:
-                        print('Score is below minimum. Please enter score between 0 and 100')
-                        continue
-
-                scores_dict.update({'Score ' + str(score + 1): input_score})
-                break
-
-            except ValueError:
-                print('Please enter scores using only numbers and one "."')
-                continue
-
-    return scores_dict
-
-'''This takes an input dictionary, totals the scores within it, then averages them.
+'''This takes an input dictionary and key, totals the scores associated with the key, then averages them.
 :input_dict: Dictionary of scores to be averaged
+:key_choice: The key chosen by the user for which scores they wanted averaged.
 :score_total: Declared variable to contain totalled scores from dictionary.
-:returns: String including average score of entries in input dictionary'''
+:returns: String including average score of entries for input dictionary key'''
 
 def average_scores(input_dict, key_choice):
     score_total = 0.0
--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -1,9 +1,9 @@
 """
-Program: assign_average.py
+Program: test_assign_average.py
 Author: Paul Elsea
 Last Modified: 06/24/2020
 
-Program to average scores from a dictionary populated by user input.
+Program to test switch_average function from assign_average.py.
 """
 
 import unittest
@@ -11,26 +11,32 @@ from unittest.mock import patch
 from more_fun_with_collections import assign_average as ave
 
 class AverageTestCases(unittest.TestCase):
+
+    '''Test to verify correct return for "A" input'''
     @patch('more_fun_with_collections.assign_average.get_input', return_value='A')
     def test_Aa_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 2.0')
 
+    '''Test to verify correct return for "B" input'''
     @patch('more_fun_with_collections.assign_average.get_input', return_value='B')
     def test_Bb_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 5.0')
 
+    '''Test to verify correct return for "C" input'''
     @patch('more_fun_with_collections.assign_average.get_input', return_value='C')
     def test_Cc_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 8.0')
 
+    '''Test to verify correct return for "D" input'''
     @patch('more_fun_with_collections.assign_average.get_input', return_value='D')
     def test_Dd_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': [0]}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 0.0')
 
+    '''Test to verify correct return for bad input'''
     @patch('more_fun_with_collections.assign_average.get_input', return_value='x')
     def test_bad_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': [0]}
