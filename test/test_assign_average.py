4ceaf3866c4860d085d1766e009db1852a3aa1ff
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-24 16:16:03 -0500

    test_assign_average.py All tests pass

--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -22,18 +22,18 @@ class AverageTestCases(unittest.TestCase):
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 5.0')
 
     @patch('more_fun_with_collections.assign_average.get_input', return_value='C')
-    def test_Bb_input(self, input):
+    def test_Cc_input(self, input):
         test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 8.0')
 
     @patch('more_fun_with_collections.assign_average.get_input', return_value='D')
     def test_Dd_input(self, input):
-        test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
+        test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': [0]}
         self.assertEqual(ave.switch_average(test_dict), 'Average score is: 0.0')
 
     @patch('more_fun_with_collections.assign_average.get_input', return_value='x')
     def test_bad_input(self, input):
-        test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': 0}
+        test_dict = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9], 'D': [0]}
         self.assertEqual(ave.switch_average(test_dict), "I'm sorry, I didn't recognize that. Please choose either A, B, C, or D.\n")
 
 
